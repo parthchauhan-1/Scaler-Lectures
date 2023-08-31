c5f7955d88f926c5047a1a275a465b7291090929
Parth Chauhan <parth.chauhan1@gmail.com>
2023-08-31 19:22:14 +0530

    let/const and var scope explained

---
+++ Module2/Class25/promiseAny.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let f1Data = fs.promises.readFile("f.txt");
+let f2Data = fs.promises.readFile("f2.txt");
+let f3Data = fs.promises.readFile("f3.txt");
+
+let allPromise = Promise.any([f1Data, f2Data, f3Data]);
+async function readAll() {
+    try {
+        allPromise.then(function (data) {
+            console.log(data + "");
+        })
+    } catch (error) {
+        // console.log("File not found")
+        console.log(error);
+    }
+}
+readAll();
