0cdf01affa5025b3542112e0603b75ef44ca0438
Parth Chauhan <parth.chauhan1@gmail.com>
2024-03-23 15:40:59 +0530

    middlewares at application level, route level and built in middlewares

--- Module4/Class2/server.js
+++ Module4/Class2/server.js
@@ -1,13 +1,43 @@
 const express = require("express");
+// exectution of middlewares depend on the sequence they're written
+// For eg. in below code, logger middleware will exeute first
+// and then dummy authorization middleware
 
 const app = express();
+// logger middleware
+app.use("/", (req, res, next) => {
+  //application level middleware
+  console.log(req.method, req.ip, req.hostname, new Date());
+  console.log(req.query);
+  next();
+});
 
-app.post("/", (req, res) => {
+// dummy authentication middleware
+const auth = (req, res, next) => {
+  console.log(req.query);
+  if (req.query.password === "123") {
+    next();
+  } else {
+    res.send("You're not authorized to view this route");
+  }
+};
+
+// app.use(auth); //app.use invokes middlware for all routes in the app.
+
+app.use(express.static("public")); // built-in function in express
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.post("/", auth, (req, res) => {
+  //route level middleware
   res.send({ type: "POST" });
 });
 
-app.get("/", (req, res) => {
+app.get("/users/:username", (req, res) => {
   res.send({ type: "GET" });
+  console.log(req.params);
 });
 
 app.put("/", (req, res) => {
